1d3b78feb294178ead604c2573eb016519eef46c
Anna Melnyk <Anna_Melnyk@epam.com>
2021-10-31 21:23:13 +0200

    Add Project6

---
+++ Project6/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback } from 'react';
+
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
+import './App.css';
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+  console.log('APP RUNNING');
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
+  }
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle button</Button>
+      <Button onClick={allowToggleHandler}>Allow toggle button</Button>
+    </div>
+  );
+}
+
+export default App;
